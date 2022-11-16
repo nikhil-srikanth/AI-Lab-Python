c487c9622b43d077b864e799fde90f8d87a4e6ad
nikhil-srikanth <95695256+nikhil-srikanth@users.noreply.github.com>
2022-11-16 09:31:05 +0530

    Update tictactoe2.py

--- tictactoe2.py
+++ tictactoe2.py
@@ -55,12 +55,11 @@ def CheckWin():
         Game = Running
 
 
-print("Tic-Tac-Toe Game Designed By Sourabh Somani")
+
 print("Player 1 [X] --- Player 2 [O]\n")
 print()
 print()
-print("Please Wait...")
-time.sleep(3)
+time.sleep(2)
 while (Game == Running):
     os.system('cls')
     DrawBoard()
