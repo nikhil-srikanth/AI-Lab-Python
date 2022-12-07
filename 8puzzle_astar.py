2c5f31a150b3e1aead79cbe3be4c50008643e8a4
nikhil-srikanth <95695256+nikhil-srikanth@users.noreply.github.com>
2022-12-07 09:44:51 +0530

    Update 8puzzle_astar.py

--- 8puzzle_astar.py
+++ 8puzzle_astar.py
@@ -1,16 +1,11 @@
 class Node:
     def __init__(self, data, level, fval):
-        """ Initialize the node with the data, level of the node and the calculated fvalue """
         self.data = data
         self.level = level
         self.fval = fval
 
     def generate_child(self):
-        """ Generate child nodes from the given node by moving the blank space
-            either in the four directions {up,down,left,right} """
         x, y = self.find(self.data, '_')
-        """ val_list contains position values for moving the blank space in either of
-            the 4 directions [up,down,left,right] respectively. """
         val_list = [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]]
         children = []
         for i in val_list:
@@ -21,8 +16,6 @@ class Node:
         return children
 
     def shuffle(self, puz, x1, y1, x2, y2):
-        """ Move the blank space in the given direction and if the position value are out
-            of limits the return None """
         if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
             temp_puz = []
             temp_puz = self.copy(puz)
@@ -34,7 +27,6 @@ class Node:
             return None
 
     def copy(self, root):
-        """ Copy function to create a similar matrix of the given node"""
         temp = []
         for i in root:
             t = []
@@ -44,7 +36,6 @@ class Node:
         return temp
 
     def find(self, puz, x):
-        """ Specifically used to find the position of the blank space """
         for i in range(0, len(self.data)):
             for j in range(0, len(self.data)):
                 if puz[i][j] == x:
@@ -53,13 +44,11 @@ class Node:
 
 class Puzzle:
     def __init__(self, size):
-        """ Initialize the puzzle size by the specified size,open and closed lists to empty """
         self.n = size
         self.open = []
         self.closed = []
 
     def accept(self):
-        """ Accepts the puzzle from the user """
         puz = []
         for i in range(0, self.n):
             temp = input().split(" ")
@@ -71,7 +60,6 @@ class Puzzle:
         return self.h(start.data, goal) + start.level
 
     def h(self, start, goal):
-        """ Calculates the different between the given puzzles """
         temp = 0
         for i in range(0, self.n):
             for j in range(0, self.n):
@@ -80,7 +68,6 @@ class Puzzle:
         return temp
 
     def process(self):
-        """ Accept Start and Goal Puzzle state"""
         print("Enter the start state matrix \n")
         start = self.accept()
         print("Enter the goal state matrix \n")
@@ -88,7 +75,6 @@ class Puzzle:
 
         start = Node(start, 0, 0)
         start.fval = self.f(start, goal)
-        """ Put the start node in the open list"""
         self.open.append(start)
         print("\n\n")
         while True:
@@ -99,7 +85,6 @@ class Puzzle:
                     print(j, end=" ")
                 print("")
                 print
-            """ If the difference between current and goal node is 0 we have reached the goal node"""
             if (self.h(cur.data, goal) == 0):
                 break
             for i in cur.generate_child():
@@ -108,7 +93,6 @@ class Puzzle:
             self.closed.append(cur)
             del self.open[0]
 
-            """ sort the opne list based on f value """
             self.open.sort(key=lambda x: x.fval, reverse=False)
 
 
